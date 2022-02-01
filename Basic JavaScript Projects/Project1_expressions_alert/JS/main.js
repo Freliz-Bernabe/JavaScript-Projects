bb7bcbaf2dea3c10cc893e6a9f76827c16b011d7
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-01-31 23:53:43 -0800

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,29 @@
+window.alert("Hello, World!") //Displays Hello, World! to the user
+
+document.write('Hello, world!'); //Text written after the alert
+
+var A = "This is a string"; //Defining a Variable A
+var A = A.fontcolor("blue"); //Using the fontcolor method on variable A
+document.write(A);
+
+var A1 = "Good Morning!"; //Defining a Variable A1
+window.alert(A1); //Displays var A1 Good Morning to the user
+
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss"); //Defining a Concatenating String
+
+var B = "Concatenated" + " String"; //Joining the texts together
+var B = B.fontcolor("red"); //Using the fontcolor method on variable B
+document.write(B)
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+    Daughter = "Penny", Son = "Zorro";
+var Dad = Dad.fontcolor("yellow"); //Using the fontcolor method on variable Dad
+document.write(Dad);
+
+3+3 //Writing an expression
+document.write(3+3); //Statement containing an expression
+
+
+
