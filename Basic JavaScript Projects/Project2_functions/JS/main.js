d873c0668711f689c95adeb2882aad4d3f444ec7
Freliz-Bernabe <96715569+Freliz-Bernabe@users.noreply.github.com>
2022-02-02 17:31:08 -0800

    Add files via upload

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+function My_First_Function(){
+    var str = "This text is red!";
+    var result = str.fontcolor("red");
+    document.getElementById("Red_Text").innerHTML=result;
+}
+
+function myFunction(){
+    var sentence="I am learning";
+    sentence +=" a lot from this book!";
+    document.getElementById("Concatenate").innerHTML=sentence;
+}
+
